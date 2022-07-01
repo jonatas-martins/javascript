2c4b55d3f831f7ff17e61a236c091bb2b602e57b
Jonatas Martins <107153976+jonatas-martins@users.noreply.github.com>
2022-07-01 11:17:24 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+/* console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
+/* var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+/* var c = 1
+do  {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 8) */
+
+//console.log('Vai começar!')
+for (var c = 1; c <= 5; c++) {
+    console.log(`Passo ${c}`)
+}
+//console.log('FIM')
+
+
